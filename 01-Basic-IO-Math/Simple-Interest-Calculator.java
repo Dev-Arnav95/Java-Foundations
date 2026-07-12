0f929299f6d9ffeda2c62c0aaf85eecec386bb0e
Dev-Studio95 <arnav95012@gmail.com>
2026-07-12 15:38:31 +0530

    Create Simple-Interest-Calculator.java

---
+++ 01-Basic-IO-Math/Simple-Interest-Calculator.java
@@ -0,0 +1,21 @@
+/*
+ * Question: Calculate Simple Interest based on user inputs.
+ * Approach: Take Principal, Rate, and Time values as double data types to account 
+ * for decimal values, then apply the formula (P * R * T) / 100.
+ */
+import java.util.*;
+
+class Simple_Interest 
+{
+    public static void main() 
+    {
+        Scanner in = new Scanner(System.in);
+        double si, p, r, t;
+        System.out.println("Enter Principal, Rate, Time...");
+        p = in.nextDouble();
+        r = in.nextDouble();
+        t = in.nextDouble();
+        si = (p * r * t) / 100;
+        System.out.println("Simple Interest=" + si);
+    }
+}
